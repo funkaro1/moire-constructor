9d093ee60b2c0499b1158b1dd22a4c2024b8618f
funkaro1 <iebarrer@uc.cl>
2024-01-06 10:18:53 -0300

    Working on export2

--- svgexport.py
+++ svgexport.py
@@ -14,7 +14,7 @@ def write(fp, points):
 def export(geo, xsize, ysize, filename):
     with open(filename, 'w') as fp:
         fp.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-        fp.write('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="{}" height="{}" viewBox="-50.0 -50.0 100 100">\n'.format(xsize, ysize))
+        fp.write('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="{}" height="{}" >\n'.format(xsize, ysize))
         fp.write('<defs>\n')
         fp.write('</defs>\n')
         for points in geo:
--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import moireclass as mc
 moire_test = mc.Moire(15,1,181)
 moire_test.setsize(20,20)
 moire_test.make("Simple")
-
+print(moire_test.poly)
 
 moire_test.export("test.svg")
